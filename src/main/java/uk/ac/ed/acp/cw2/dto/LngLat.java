b98543f5a4655ee06a4ee58d68d259ca2c2a0ca5
brmyror <rorybryan@icloud.com>
2025-12-01 04:03:27 +0000

    pls work

--- src/main/java/uk/ac/ed/acp/cw2/dto/LngLat.java
+++ src/main/java/uk/ac/ed/acp/cw2/dto/LngLat.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 /**
  * DTO for a longitude and latitude position.
@@ -11,6 +13,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LngLat {
     @JsonProperty("lng")
     private Double lng;
